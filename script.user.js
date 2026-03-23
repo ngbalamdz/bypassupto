09028f1dd02d7413dec424429fa70b2a2d2a2ef0
ngbalamdz <lamncb2000@gmail.com>
2026-03-23 11:08:39 +0700

    Update script requirement URL for bypassing UptoLink

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Bypass UptoLink for cuttay user
-// @require      https://github.com/phatnottaken51/bypass-upto-link/raw/refs/heads/main/source.js
+// @require      https://github.com/ngbalamdz/bypassupto/raw/refs/heads/main/source.js
 // @match        *://*/*
 // @author       PhatNotTaken
 // @grant        GM_xmlhttpRequest
